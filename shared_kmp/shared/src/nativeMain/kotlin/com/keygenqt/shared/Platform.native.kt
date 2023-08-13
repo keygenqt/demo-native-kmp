e53852bc303463386f72acfc07e8ccb35b4d7b43
Vitaliy Zarubin <v.zarubin@omp.ru>
2023-08-13 15:52:11 +0300

    mini fix

--- shared_kmp/shared/src/nativeMain/kotlin/com/keygenqt/shared/Platform.native.kt
+++ shared_kmp/shared/src/nativeMain/kotlin/com/keygenqt/shared/Platform.native.kt
@@ -7,7 +7,7 @@ import io.ktor.client.request.*
 import kotlinx.coroutines.runBlocking
 
 fun user(name: String): String {
-    // return "You name: $name"
+//     return "You name: $name"
     val client = HttpClient(Curl)
     return runBlocking {
         client.get("https://api.github.com/users/$name").body()
